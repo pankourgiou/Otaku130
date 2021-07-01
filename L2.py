86b6c56857203c652fa3f290e4ae67fbde4bddb3
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 09:12:43 +0300

    Update and rename Turing.rb to L2.py

---
+++ L2.py
@@ -0,0 +1,3 @@
+
+for _ in range(1000000):
+    print("Debian Gentoo  Ubuntu Linux Mint Red Hat Enterprise Linux CentOS Fedora Kali Linux.")
--- Turing.rb
+++
@@ -1,3 +0,0 @@
-10000000000000000000000000.times do
-     puts "Enigma vs Turing machine...???...Hahaha"
-end
